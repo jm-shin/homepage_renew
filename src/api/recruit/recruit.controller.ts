4fb7621451258e8de55b190e9a85fdaf650949e2
jongmin <ace4463@gmail.com>
2022-07-26 17:15:19 +0900

    modify: UseFilters location

--- src/api/recruit/recruit.controller.ts
+++ src/api/recruit/recruit.controller.ts
@@ -5,42 +5,40 @@ import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 
 
 @Controller('recruit')
+@UseFilters(HttpExceptionFilter)
 export class RecruitController {
-  constructor(private readonly recruitService: RecruitService) {
+  constructor(
+    private readonly recruitService: RecruitService,
+  ) {
   }
 
   //채용 공고 - 등록
   @Post()
   @HttpCode(200)
-  @UseFilters(new HttpExceptionFilter())
   createRecruit(@Body() body: RecruitDto) {
     return this.recruitService.insertRecruit(body);
   }
 
   // 채용 공고 - 수정
   @Put('/:id')
-  @UseFilters(new HttpExceptionFilter())
   updateRecruit(@Param('id') id: string, @Body() body: RecruitDto) {
     return this.recruitService.updateRecruit(id, body);
   }
 
   // 채용 공고 - 삭제
   @Delete('/:id')
-  @UseFilters(new HttpExceptionFilter())
   removeRecruit(@Param('id') id: string) {
     return this.recruitService.deleteRecruit(id);
   }
 
   // 채용 공고 - 상세보기
   @Get('/detail/:id')
-  @UseFilters(new HttpExceptionFilter())
   getRecruit(@Param('id') id: string) {
     return this.recruitService.findOneRecruit(id);
   }
 
   // 채용 공고 - 목록
   @Get('/list')
-  @UseFilters(new HttpExceptionFilter())
   getRecruitList() {
     return this.recruitService.findAllRecruit();
   }
